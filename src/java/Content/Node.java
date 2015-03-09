593aa8f381cc87f68e2625af076df9cd04e3f9fb
nitlab <kkatsalis@gmail.com>
2015-03-09 19:29:20 +0200

    Manager Uploaded

---
+++ src/java/Content/Node.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Content;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * REST Web Service
+ *
+ * @author nitlab
+ */
+@Path("/node")
+public class Node {
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public Node() {
+    }
+
+    /**
+     * Retrieves representation of an instance of Content.Node
+     * @return an instance of java.lang.String
+     */
+   
+
+    @GET
+    @Path("{nodeID}/{action}")
+    @Produces("application/json")
+    public String actionOnNode(@PathParam("nodeID") String _nodeID,@PathParam("action") String _action) {
+        String jsonObj="";
+        String command="";
+        String address="nitlab3.inf.uth.gr";
+        String slice="kostas";
+        String node=_nodeID;
+        String action=_action;
+        
+        if("imageLoad".equals(action))
+            command="omf load -i baseline.ndz -t omf.nitos."+node;
+        else if("on".equals(action)||"reboot".equals(action))
+            command="omf tell -a "+action+" -t omf.nitos."+node;
+      
+        
+        String response=Utilities.remoteExecution(address,slice,command);
+         
+         if("imageLoad".equals(action)){
+              if(response.toLowerCase().contains("1 node successfully imaged".toLowerCase()))
+                      response="success";
+              else 
+                  response="failure";
+         }
+         else if("on".equals(action)||"reboot".equals(action)){
+               if(response.toLowerCase().contains("finished after".toLowerCase()))
+                     response="success";
+               else 
+                     response="failure";
+               }
+         
+            jsonObj="{\"node\":\""+_nodeID+"\",\"action\":\""+_action+"\",\"status\":\""+response+"\"}";
+            
+           
+         return jsonObj;
+    //    return "<html lang=\"en\"><body><h1>"+response+"</body></h1></html>";
+    }
+    /**
+     * PUT method for updating or creating an instance of Node
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("application/json")
+    public void putJson(String content) {
+    }
+}
---
+++ src/java/Content/Utilities.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Content;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+
+/**
+ *
+ * @author nitlab
+ */
+public class Utilities {
+    
+    public static String remoteExecution(String address,String slice,String command){
+    
+        
+       String response="";
+        String image="baseline.ndz";
+       
+        try{
+            
+             JSch jsch=new JSch();
+
+            
+             Session session=jsch.getSession(slice, address, 22);
+
+             jsch.setKnownHosts("/home/nitlab/.ssh/known_hosts");
+             jsch.addIdentity("/home/nitlab/.ssh/id_rsa");
+   
+   // If two machines have SSH passwordless logins setup, the following line is not needed:
+             //    session.setPassword("YOURPASSWORD");
+                session.setConfig("StrictHostKeyChecking", "no");
+                 session.connect();
+
+
+                 Channel channel=session.openChannel("exec");
+                 ((ChannelExec)channel).setCommand(command);
+
+                 //channel.setInputStream(System.in);
+                 channel.setInputStream(null);
+                 ((ChannelExec)channel).setErrStream(System.err);
+
+                 InputStream in=channel.getInputStream();
+                 channel.connect();
+                  response = IOUtils.toString(in);
+
+                 byte[] tmp=new byte[1024];
+                 while(true){
+                   while(in.available()>0){
+                     int i=in.read(tmp, 0, 1024);
+                     if(i<0)break;
+                     System.out.print(new String(tmp, 0, i));
+                   }
+                   if(channel.isClosed()){
+                     System.out.println("exit-status: "+channel.getExitStatus());
+                     break;
+                   }
+                   try{Thread.sleep(1000);}catch(Exception ee){ee.printStackTrace();}
+                 }
+                 channel.disconnect();
+                 session.disconnect();
+               }
+               catch(Exception e){
+                 System.out.println(e);
+              }
+ 
+        return response;
+  
+          }       //end main
+    
+}
